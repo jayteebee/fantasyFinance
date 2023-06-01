d31d64f3f3f65f4d3527a64687d2cf3658b08d6a
Jethro_Block <jethroblock22@gmail.COM>
2023-06-01 11:06:05 +0100

    Add config lines to reg + sessions controller
    Comments in user serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,3 +2,13 @@ class UserSerializer
   include JSONAPI::Serializer
   attributes :id, :email, :created_at
 end
+
+
+
+
+
+# ACCESS SERIALIZER DATA FOR SINGLE RECORD
+
+# UserSerializer.new(resource).serializable_hash[:data][:attributes].to_json
+# And multiple records by,
+# UserSerializer.new(resource).serializable_hash[:data].map{|data| data[:attributes].to_json}
