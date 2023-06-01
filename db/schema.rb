3863de6737a76b343e6dbbaf9b1e301f98c456e0
Jethro_Block <jethroblock22@gmail.COM>
2023-06-01 10:37:14 +0100

    Sort non-unique email issue

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_31_153602) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_01_091229) do
   create_table "holdings", force: :cascade do |t|
     t.integer "user_id", null: false
     t.integer "stock_id", null: false
@@ -72,6 +72,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_31_153602) do
     t.integer "balance"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   create_table "watchlist_stocks", force: :cascade do |t|
