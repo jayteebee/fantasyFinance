93758282ba59c110f336757ebf3f5f5f575e7aa6
Jethro_Block <jethroblock22@gmail.COM>
2023-06-22 14:49:18 +0100

    Cors modifications

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,13 +1,17 @@
 # config/initializers/cors.rb
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
-    allow do
-      origins 'http://localhost:3000'
-      resource(
-        '*',
-        headers: :any,
-        expose: ['access-token', 'expiry', 'token-type', 'Authorization'],
-        methods: [:get, :patch, :put, :delete, :post, :options, :show, :head]
-       
-      )
+  allow do
+    if Rails.env.production?
+      origins 'https://fantasy-finance-fe-416b523c0162.herokuapp.com'  # Heroku frontend URL
+    else
+      origins 'http://localhost:3000'  # localhost URL for development
     end
+
+    resource(
+      '*',
+      headers: :any,
+      expose: ['access-token', 'expiry', 'token-type', 'Authorization'],
+      methods: [:get, :patch, :put, :delete, :post, :options, :show, :head]
+    )
   end
+end
